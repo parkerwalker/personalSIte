910f5f1a8a3a3a16f1e24446dcac1cb28dd1e59d
Parker Walker <parkerwalker12@gmail.com>
2017-08-10 16:24:40 -0500

    added links

--- public/script/app.js
+++ public/script/app.js
@@ -1,1 +1,7 @@
 console.log('js');
+var app = angular.module('app', ['ngRoute']);
+
+app.controller('MainController', function($location){
+  console.log('in MainController');
+  
+});//end MainController
