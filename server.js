9ed268540bf204d46858b08aad2c33ff5587ab19
Parker Walker <parkerwalker12@gmail.com>
2017-08-10 14:47:07 -0500

    skeleton files

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var index = require('./modules/routes/index');
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded( { extended: true } ) );
 app.use(bodyParser.json());
+
+app.use('/', index);
 
 var port = process.env.PORT || 4000;
 
