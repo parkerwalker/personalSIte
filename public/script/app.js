7bf8a3c6cc8100c20ac55bcb554b64a112717e47
Parker Walker <parkerwalker12@gmail.com>
2017-08-10 16:44:02 -0500

    added links with tabs and materialize

--- public/script/app.js
+++ public/script/app.js
@@ -3,5 +3,8 @@ var app = angular.module('app', ['ngRoute']);
 
 app.controller('MainController', function($location){
   console.log('in MainController');
-  
+
+  var vm = this;
+
+
 });//end MainController
